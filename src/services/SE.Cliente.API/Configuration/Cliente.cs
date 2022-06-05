572a3d6dbc1752dcc4244b4d95711b58f1756153
Erick Sales <ericksalec@gmail.com>
2022-06-05 15:30:18 -0300

    feat: definindo as entidades e validacoes para Cliente

---
+++ src/services/SE.Cliente.API/Configuration/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using SE.Core.DomainObjects;
+
+namespace SE.Cliente.API.Configuration
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public string Nome { get; set; }
+        public Email Email { get; set; }
+        public Cpf Cpf { get; set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        // EF Relation
+        public Cliente() { }
+
+        public Cliente(Guid id, string nome, string cpf, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+    }
+}
---
+++ src/services/SE.Cliente.API/Configuration/Endereco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SE.Core.DomainObjects;
+
+namespace SE.Cliente.API.Configuration
+{
+    public class Endereco : Entity
+    {
+        public string Longradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        // EF Relation
+        public Cliente Cliente { get; protected set; }
+
+        public Endereco(string longradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Longradouro = longradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+    }
+}
