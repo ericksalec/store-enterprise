d70a53d6021876f7fc8483dc19db620682f2c3bc
Erick Sales <ericksalec@gmail.com>
2022-06-05 17:37:00 -0300

    feat: implementando CQRS command

---
+++ src/building blocks/SE.Core/Messages/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.Results;
+using Microsoft.VisualBasic;
+
+namespace SE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
---
+++ src/services/SE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SE.Core.Messages;
+
+namespace SE.Clientes.API.Application.Commands
+{
+    public class RegistrarClienteCommand : Command
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public RegistrarClienteCommand(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
