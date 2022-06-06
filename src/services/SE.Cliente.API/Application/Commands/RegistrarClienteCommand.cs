1048cf6d56dcf81478a57ecea72bc13f2d2c2ee4
Erick Sales <ericksalec@gmail.com>
2022-06-06 18:12:49 -0300

    feat: validacao de comandos

--- src/services/SE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ src/services/SE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentValidation;
 using SE.Core.Messages;
 
 namespace SE.Clientes.API.Application.Commands
@@ -18,5 +19,45 @@ namespace SE.Clientes.API.Application.Commands
             Email = email;
             Cpf = cpf;
         }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não foi informado.");
+
+                RuleFor(c => c.Cpf)
+                    .Must(TerCpfValido)
+                    .WithMessage("O CPF informado não é válido.");
+
+                RuleFor(c => c.Email)
+                    .Must(TerEmailValido)
+                    .WithMessage("O Email informado não é válido.");
+            }
+
+            protected static bool TerCpfValido(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+
+            protected static bool TerEmailValido(string email)
+            {
+                return Core.DomainObjects.Email.Validar(email);
+            }
+        }
+
     }
+
 }
