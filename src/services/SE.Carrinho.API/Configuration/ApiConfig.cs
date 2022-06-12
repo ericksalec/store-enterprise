8bbac99fd64bf09868cd4debb92f71c489a9c3f1
Erick Sales <ericksalec@gmail.com>
2022-06-12 15:52:53 -0300

    feat: configuracoes de dependencia - api carrinho

---
+++ src/services/SE.Carrinho.API/Configuration/ApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SE.WebAPI.Core.Identidade;
+
+namespace SE.Carrinho.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
---
+++ src/services/SE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using SE.WebAPI.Core.Controllers;
+
+namespace SE.Carrinho.API.Controllers
+{
+    [Authorize]
+    public class CarrinhoController : MainController
+
+    {
+    }
+}
