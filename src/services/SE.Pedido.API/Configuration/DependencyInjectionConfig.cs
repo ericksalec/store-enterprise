d37996b76295a2e658d908071b2cbcd0eaffb90c
Erick Sales <ericksalec@gmail.com>
2022-06-16 04:33:26 -0300

    feat: implementando controller da api de pedidos

--- src/services/SE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/SE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -3,6 +3,8 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SE.Core.Mediator;
+using SE.Pedidos.API.Application.Commands;
+using SE.Pedidos.API.Application.Events;
 using SE.Pedidos.API.Application.Queries;
 using SE.Pedidos.Domain;
 using SE.Pedidos.Domain.Pedidos;
@@ -20,14 +22,23 @@ namespace SE.Pedidos.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
         }
+
 
     }
 }
